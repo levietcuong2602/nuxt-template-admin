7ca112012c0a2fa524d3e564d769c8817f7eda94
cuonglv <levietcuong0297@gmail.com>
2019-09-23 16:20:23 +0700

    create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# vbee_sach_noi_web
+### Install
+yarn install
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home-container">this is page home</div>
+</template>
+<script>
+export default {
+  name: "Home"
+};
+</script>
